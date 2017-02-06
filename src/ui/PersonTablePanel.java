7d38310f82266ca3ea90c73c4e65df6097887f9a
Brian Gibson <bggibson@hotmail.com>
2017-02-06 15:29:53 +0100

    Add popup menu

--- src/ui/PersonTablePanel.java
+++ src/ui/PersonTablePanel.java
@@ -4,14 +4,36 @@ import domain.PersonMessage;
 
 import javax.swing.*;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 
 class PersonTablePanel extends JPanel {
 
     private final PersonTableModel personTableModel = new PersonTableModel();
+    private final JTable tablePanel = new JTable(personTableModel);
+    private final JPopupMenu popupMenu = new JPopupMenu();
 
     PersonTablePanel() {
+        setupPopupMenu();
+        addComponents();
+    }
+
+    private void setupPopupMenu() {
+        JMenuItem deleteRowMenuItem = new JMenuItem("Delete row");
+        popupMenu.add(deleteRowMenuItem);
+
+        tablePanel.addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseClicked(MouseEvent e) {
+                if (e.getButton() == MouseEvent.BUTTON3)
+                    popupMenu.show(tablePanel, e.getX(), e.getY());
+            }
+        });
+    }
+
+    private void addComponents() {
         setLayout(new BorderLayout());
-        JTable tablePanel = new JTable(personTableModel);
+        add(popupMenu);
         add(new JScrollPane(tablePanel), BorderLayout.CENTER);
     }
 
