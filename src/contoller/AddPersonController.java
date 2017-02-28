157418dfd6b57ac63e21962154cf80aee503ac58
Brian Gibson <bggibson@hotmail.com>
2017-02-28 16:40:43 +0100

    Rename RequestBuilderImpl to RequestBuilder Refactor

--- src/contoller/AddPersonController.java
+++ src/contoller/AddPersonController.java
@@ -3,7 +3,7 @@ package contoller;
 import other.Controller;
 import other.View;
 import requestor.Request;
-import requestor.RequestBuilderImpl;
+import requestor.RequestBuilder;
 import requestor.UseCase;
 import requestor.UseCaseFactory;
 import responder.Presenter;
@@ -16,8 +16,8 @@ public class AddPersonController implements Controller {
     private final Presenter presenter;
     private final View view;
 
-    public AddPersonController(RequestBuilderImpl request, Map<Integer, Object> args, UseCaseFactory useCase, Presenter presenter, View view) {
-        this.request = request.get("AddPersonRequest", args);
+    public AddPersonController(RequestBuilder request, Map<Integer, Object> args, UseCaseFactory useCase, Presenter presenter, View view) {
+        this.request = request.make("AddPersonRequest", args);
         this.useCase = useCase.make("AddPersonUseCase", presenter);
         this.presenter = presenter;
         this.view = view;
