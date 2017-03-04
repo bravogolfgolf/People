cb3d56b6c46a04bfdf4a878d877fb4a81f8b7bee
Brian Gibson <bggibson@hotmail.com>
2017-03-04 16:59:18 +0100

    Implement ExportPresenter & ExportView Refactor

--- src/controller/ExportController.java
+++ src/controller/ExportController.java
@@ -1,7 +1,7 @@
 package controller;
 
 import builderfactory.*;
-import responder.RefreshResponder;
+import responder.ExportResponder;
 import responder.View;
 
 import java.util.Map;
@@ -9,19 +9,19 @@ import java.util.Map;
 public class ExportController extends Controller {
     private final Request request;
     private final UseCase useCase;
-    private final RefreshResponder presenter;
+    private final ExportResponder responder;
     private final View view;
 
-    public ExportController(RequestBuilder request, Map<Integer, Object> args, UseCaseFactory useCase, RefreshResponder presenter, View view) {
+    public ExportController(RequestBuilder request, Map<Integer, Object> args, UseCaseFactory useCase, ExportResponder responder, View view) {
         this.request = request.make("Export", args);
-        this.useCase = useCase.make("Export", presenter);
-        this.presenter = presenter;
+        this.useCase = useCase.make("Export", responder);
+        this.responder = responder;
         this.view = view;
     }
 
     @Override
     public Object execute() {
         useCase.execute(request);
-        return 1;
+        return view.generateView(responder.getViewModel());
     }
 }
